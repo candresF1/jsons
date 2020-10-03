e9e7167bd454345988420a5049edfe6e3c3be324
Camilo Andrés Pérez Cubillos <64588755+candresF1@users.noreply.github.com>
2020-10-02 20:53:49 -0500

    asdf

--- TipoDocs.js
+++ TipoDocs.js
@@ -1,20 +1,9 @@
-{
-  TipoDoc = [
-    {
-      label: 'Tommy Wiseau',
-      value: 1
-    }, 
-    {
-      label: 'Arnold Schwarzenneger',
-      value: 2
-    }, 
-    {
-      label: 'Donald Glover',
-      value: 3
-    },
-    {
-      label: 'Emma Stone',
-      value: 4
-    }
-  ]
-}
+{"Cat": [
+  {"key": "a", "title": "Bangladesh", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/bangladesh.jpg"},
+  {"key": "b", "title": "Sports", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/sports.jpg"},
+  {"key": "c", "title": "Politics", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/politics.jpg"},
+  {"key": "d", "title": "Entertainment", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/entertainment.png"},
+  {"key": "e", "title": "Economics", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/economics.jpg"},
+  {"key": "f", "title": "Technology", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/technology.jpg"},
+  {"key": "g", "title": "Others", "image": "https://raw.githubusercontent.com/mudassir21/server/master/img/m.jpg"}
+]}
