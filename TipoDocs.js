0390e414992f8afdc2f1a9032354358428dbb819
Camilo Andrés Pérez Cubillos <64588755+candresF1@users.noreply.github.com>
2020-10-02 19:30:33 -0500

    AddingTipoDocFile

---
+++ TipoDocs.js
@@ -0,0 +1,18 @@
+[
+  {
+    label: 'Tommy Wiseau',
+    value: 1
+  }, 
+  {
+    label: 'Arnold Schwarzenneger',
+    value: 2
+  }, 
+  {
+    label: 'Donald Glover',
+    value: 3
+  },
+  {
+    label: 'Emma Stone',
+    value: 4
+  }
+]
